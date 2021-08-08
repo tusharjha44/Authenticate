21833cbff542449cf096dccda9f0b75b04cec287
tusharjha44 <85125680+tusharjha44@users.noreply.github.com>
2021-08-09 02:00:33 +0530

    Level 3 Hashing

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const { urlencoded } = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express(); 
 
-console.log(process.env.API_KEY);
+console.log(md5("12345"));
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
@@ -23,7 +23,6 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, {secret:  process.env.SECRET, encryptedFields: ["password"]});
 
 const User = mongoose.model("User",userSchema);
 
@@ -43,7 +42,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err){
@@ -57,7 +56,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username},function(err,foundUser){
         if(err){
